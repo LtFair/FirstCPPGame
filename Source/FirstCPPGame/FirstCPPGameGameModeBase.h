b58b5e2c0d27d248a64266016ef310bcfcb6ed6e
Tom Radford <tomradford596@gmail.com>
2020-06-24 23:27:43 -0400

    Initial commit

---
+++ Source/FirstCPPGame/FirstCPPGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstCPPGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIRSTCPPGAME_API AFirstCPPGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
